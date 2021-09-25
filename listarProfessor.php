ecfc155bafdb9f55b2ce0fa2d190b69084610b70
gandalferr <91101400+gandalferr@users.noreply.github.com>
2021-09-24 23:15:11 -0300

    Ajuste listaProfessor

--- listarProfessor.php
+++ listarProfessor.php
@@ -10,11 +10,10 @@
     
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 
-    <link href="css/meu-estilo-original.css" rel="stylesheet">
-    
     <style>
         * {
             font-family: 'Open Sans', sans-serif;
+            font-size: 20px;
         }
         
         main h2 {
@@ -26,16 +25,28 @@
 
         table {
             margin: 0 auto;
+            border-spacing: 0;
         }
 
-        tr, td {
+        th {
+            text-align: center;
             color: blue;
             border-bottom: 1px blue solid;
         }
 
-        span:hover, td:hover {
-            background-color: blue;
-            color: #ffffff;
+        th, tr {
+            padding: 12px 0;
+            text-align: center;
+            vertical-align: middle;
+        }
+
+        td:nth-child(2) {
+           text-align: left;
+        }
+
+        tbody > tr:hover {
+            color: blue;
+            background-color: #e6eed5;
         }
 
     </style>
@@ -47,75 +58,75 @@
 
         <h2>Listar Professores</h2>
 
-        <table class="lista-funcionarios">
-            <thead>
-                <tr>
-                    <th>Matrícula</th>
-                    <th>Professor</th>
-                    <th colspan="2">Ações</th>
-                </tr>
-            </thead>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Matrícula</th>
+                        <th>Professor</th>
+                        <th colspan="2">Ações</th>
+                    </tr>
+                </thead>
 
-            <?php
+                <?php
 
-                $host = "localhost";
-                $dbUsuario = "root";
-                $dbSenha = "";
-                $dbSchema = "lista_chamadas_v2";
-                
-                /* conectar o bd */
-                $con = mysqli_connect($host, $dbUsuario, $dbSenha, $dbSchema);
-                if (mysqli_connect_errno()) { // verifica se a conexao foi efetuado com sucesso
-                    $msgBD = mysqli_connect_error();
-                    echo "Falha na conexao com o BD: $msgBD";
-                    exit();
-                }
-           
-                /* listar os professores da tabela professor */
-                $consulta  = "SELECT matricula, nome FROM professores";
-                $resultado = mysqli_query($con, $consulta);
-                
-                if ($resultado) {
+                    $host = "localhost";
+                    $dbUsuario = "root";
+                    $dbSenha = "";
+                    $dbSchema = "lista_chamadas_v2";
+                    
+                    /* conectar o bd */
+                    $con = mysqli_connect($host, $dbUsuario, $dbSenha, $dbSchema);
+                    if (mysqli_connect_errno()) { // verifica se a conexao foi efetuado com sucesso
+                        $msgBD = mysqli_connect_error();
+                        echo "Falha na conexao com o BD: $msgBD";
+                        exit();
+                    }
+            
+                    /* listar os professores da tabela professor */
+                    $consulta  = "SELECT matricula, nome FROM professores";
+                    $resultado = mysqli_query($con, $consulta);
+                    
+                    if ($resultado) {
 
-                    while ( $linha = mysqli_fetch_array($resultado) ) {
-                        $matricula = $linha['matricula'];
-                        $nome = $linha['nome'];
-                        $atualizarProfessor = "atualizarProfessor.php?matricula=" . $matricula;
-            ?>
+                        while ( $linha = mysqli_fetch_array($resultado) ) {
+                            $matricula = $linha['matricula'];
+                            $nome = $linha['nome'];
+                            $atualizarProfessor = "atualizarProfessor.php?matricula=" . $matricula;
+                ?>
 
-                <tr>
-                    <td>
-                        <?=$matricula?>
-                    </td>    
-                    <td>
-                        <?=$nome?>
-                    </td>
-                    <td>
-                        <a href="atualizarProfessor.php?matricula=<?=$matricula?>" >
-                        <span class="material-icons">update</span></a> 
-                    </td>
-                    <td>
-                        <a href="excluirProfessor.php?matricula=<?=$matricula?>" >
-                        <span class="material-icons">delete_forever</span></a> 
-                    </td>
+                    <tr>
+                        <td>
+                            <?=$matricula?>
+                        </td>    
+                        <td>
+                            <?=$nome?>
+                        </td>
+                        <td>
+                            <a href="atualizarProfessor.php?matricula=<?=$matricula?>" >
+                            <span class="material-icons">update</span></a> 
+                        </td>
+                        <td>
+                            <a href="excluirProfessor.php?matricula=<?=$matricula?>" >
+                            <span class="material-icons">delete_forever</span></a> 
+                        </td>
 
-                </tr>
+                    </tr>
 
-            <?php
+                <?php
+                        }
+
                     }
 
-                }
-
-                /* fecha o objeto query */
-                if (is_object($resultado)) {
-                    mysqli_free_result($resultado);
-                }
-                /* fecha a conexao com o BD*/
-                mysqli_close($con);
-            ?>
-            <!-- fim bloco PHP com HTML -->
-        
-        </table>
+                    /* fecha o objeto query */
+                    if (is_object($resultado)) {
+                        mysqli_free_result($resultado);
+                    }
+                    /* fecha a conexao com o BD*/
+                    mysqli_close($con);
+                ?>
+                <!-- fim bloco PHP com HTML -->
+            
+            </table>
 
     </main>
     <!-- fim do codigo HTML -->
